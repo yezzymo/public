834380855b70764a0c28c6c0eb43c56033074241
yezzy.mo <contact@mobeygi.com>
2023-11-29 21:48:07 +0100

    upload files

---
+++ app.js
@@ -0,0 +1,115 @@
+/* 
+function updateDisplay() {
+
+  const item = document.getElementsByTagName("li");
+  const listBox = document.getElementById("notfications");
+  const inputField = document.getElementById("addTask");
+
+
+  if (item.length == 0) {
+   listBox.style.display = "none";
+   console.log("empty")
+  }  else  {listBox.style.display = "grid";
+console.log("not Empty")}
+renderList();
+};
+
+document.addEventListener("DOMContentLoaded", updateDisplay);
+updateDisplay();
+*/
+
+
+
+document.getElementById("notification-box").addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  const list = document.getElementById("myList");
+  const itemsArray = JSON.parse(localStorage.getItem('itemsArray')) || [];
+  const inputField = document.getElementById("addTask");
+  const inputValue = inputField.value.trim();
+
+  if (inputValue !== "") {
+    // Add the input value to the array along with a checked property
+    itemsArray.push({ text: inputValue, checked: false });
+
+    // Save the updated array to local storage
+    localStorage.setItem('itemsArray', JSON.stringify(itemsArray));
+
+    // Clear the input field
+    inputField.value = "";
+
+    // Render the updated list
+    renderList();
+  }
+});
+
+function renderList() {
+  const list = document.getElementById("myList");
+  const itemsArray = JSON.parse(localStorage.getItem('itemsArray')) || [];
+
+  list.innerHTML = ""; // Clear the current list
+
+  // Loop through the array and create list items
+  itemsArray.forEach(function(item, index) {
+    const listItem = document.createElement("li");
+    const customInput = document.createElement("input");
+    const inputParagraph = document.createElement("p");
+    customInput.type = "checkbox";
+    customInput.id = `customCheckbox_${index}`;
+    customInput.checked = item.checked; // Set the checkbox state
+
+    if (item.checked) {
+      customInput.checked = true;
+      inputParagraph.classList.add("inputChecked");
+    } else {
+      customInput.checked = false;
+      inputParagraph.classList.remove("inputChecked");
+    }
+    
+    
+    customInput.addEventListener('click', function() {
+      itemsArray[index].checked = customInput.checked;
+
+      if (customInput.checked) {
+        item.class = "inputChecked";
+
+      } else {
+        item.class = "";
+      }
+
+      // Update local storage
+      localStorage.setItem('itemsArray', JSON.stringify(itemsArray));
+
+      if (customInput.checked) {
+        inputParagraph.classList.add("inputChecked");
+      } else {
+        inputParagraph.classList.remove("inputChecked");
+      }
+    });
+
+
+    const removeItem = document.createElement("i");
+    removeItem.classList.add("far", "fa-trash-alt", "fa-xl");
+    removeItem.addEventListener('click', function() {
+      itemsArray.splice(index, 1);
+
+      // Update local storage
+      localStorage.setItem('itemsArray', JSON.stringify(itemsArray));
+
+      // Re-render the list
+      renderList();
+    });
+
+    listItem.classList.add("message-box", "blue", "user-message");
+    inputParagraph.innerText = item.text;
+    // Append the custom checkbox and remove button to the list item
+    listItem.appendChild(customInput);
+    listItem.appendChild(inputParagraph);
+    listItem.appendChild(removeItem);
+    list.prepend(listItem); // Append the list item to the unordered list
+  });
+}
+
+// Initially render the list when the page loads
+renderList();
+
